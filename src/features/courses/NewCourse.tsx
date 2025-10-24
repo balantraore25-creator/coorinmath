abbaaa49e4e04af995ccc6f110091d0a99015180
BALAN <balantraore@hotmail.com>
2025-10-24 22:01:56 +0200

    Ready to deploy

--- src/features/courses/NewCourse.tsx
+++ src/features/courses/NewCourse.tsx
@@ -32,26 +32,3 @@ export default NewCourse
 
 
 
-
-/*import { useSelector } from 'react-redux'
-import type { RootState } from '../../app/store' // adapte le chemin si besoin
-import { selectAllUsers } from '../users/usersApiSlice'
-import NewCourseForm from './NewCourseForm'
-import { Alert } from '@chakra-ui/react'
-
-const NewCourse: React.FC = () => {
-  const users = useSelector((state: RootState) => selectAllUsers(state))
-
-  if (!users?.length) {
-    return (
-       <Alert.Root status="info" title="This is the alert title">
-      <Alert.Indicator />
-      <Alert.Title>Aucun cours disponible pour le moment</Alert.Title>
-    </Alert.Root>
-    )
-  }
-
-  return <NewCourseForm users={users} />
-}
-
-export default NewCourse*/
