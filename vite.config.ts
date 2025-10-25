ccecf641e833b881afe41f16987d41a83c009de1
BALAN <balantraore@hotmail.com>
2025-10-25 05:00:35 +0200

    Ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vite.dev/config/
 export default defineConfig({
    plugins: [react(), tsconfigPaths()],
+    base: '/', // ✅ important pour Render
     build: {
     outDir: 'dist', // par défaut
   },
